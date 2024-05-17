472091c76f5897b204a724e7d17d350bf824a013
elianrichard <elianrichards@gmail.com>
2024-05-17 17:09:30 +0700

    Initial Commit

---
+++ WorkcareTimer/WorkcareTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkcareTimerApp.swift
+//  WorkcareTimer
+//
+//  Created by Elian Richard on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkcareTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
